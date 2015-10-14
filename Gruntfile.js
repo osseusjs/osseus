a287cb1a8703df790acc46e331ab8cc25fd982e8
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2015-10-13 21:19:41 -0500

    Includding to and from db updates, moving the includes and the layouts to the theme, and adding a test for a config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,16 @@
+'use strict';
+
+yaml = require('js-yaml');
+fs   = require('fs');
+
+// Get document, or throw exception on error
+
 module.exports = function(grunt) {
+  try {
+    global.config = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
+  } catch (e) {
+    throw new Error('No config file found!');
+  }
+
   require('load-grunt-config')(grunt);
 };
